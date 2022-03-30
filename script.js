8e43f6519f1db0702770c050132d17a969e10126
DDeerrpp <raidon69869@icloud.com>
2022-03-29 21:52:29 -0700

    finished

---
+++ script.js
@@ -0,0 +1,30 @@
+const rollBtn = document.getElementById("dice");
+var num = 1;
+rollBtn.addEventListener('click', () => {
+    rollForAdvice();
+})
+
+function rollForAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(res => res.json())
+    .then((data) =>  {
+        
+        if(num == data.slip.id) {
+            rollForAdvice();
+        } else {
+            document.getElementById("advice").innerText = `"${data.slip.advice}"`;
+            document.getElementById("advice-id").innerText = `ADVICE #${data.slip.id}`;
+            num = data.slip.id;
+        }
+        
+    });
+}
+rollForAdvice();
+
+rollBtn.addEventListener('mouseover', () => {
+    var dice = document.querySelector("#dice img");
+    dice.classList.add("img-rotate");
+    const timeout = setTimeout(() => {
+        dice.classList.remove("img-rotate");
+    }, 1000);
+})
